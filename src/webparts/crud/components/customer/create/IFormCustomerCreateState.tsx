dab240cda7aff285baae1185502530bd0b72cd66
Pedro Miguel Mourão Martins <miguelrox@msn.com>
2019-09-01 18:55:26 +0100

    add operation model create customer

---
+++ src/webparts/crud/components/customer/create/IFormCustomerCreateState.tsx
@@ -0,0 +1,9 @@
+import { ICustomer } from "../Models/ICustomer";
+import { ICustomersDataProvider } from "../sharePointDataProvider/ICustomersDataProvider";
+
+export interface IFormCustomerCreateState {
+  isBusy: boolean;
+  customer: ICustomer;
+  messageSended: boolean;
+  customersDataProvider:ICustomersDataProvider;
+}
